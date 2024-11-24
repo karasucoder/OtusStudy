2e5ff7b065d1af8d7762c51a4ac00a0207cdb88d
Lyudmila Maslova <m.maslova.1994@gmail.com>
2024-11-24 15:45:06 +0300

    solution rename

--- BotInteractiveMenuApp/Program.cs
+++ BotInteractiveMenuApp/Program.cs
@@ -1,197 +1,192 @@
-﻿using System.Diagnostics.Metrics;
-using System.Net.Quic;
-using System.Numerics;
+﻿namespace BotInteractiveMenuApp;
 
-namespace BotInteractiveMenuApp
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string userName = null;
+
+        while (true)
         {
-            string userName = null;
+            Greeting(userName);
 
-            while (true)
+            string userCommand = Console.ReadLine();
+
+            if (userCommand.StartsWith("/echo"))
             {
-                Greeting(userName);
-
-                string userCommand = Console.ReadLine();
-
-                if (userCommand.StartsWith("/echo"))
-                {
-                    Echo(userName, userCommand);
-                    continue;
-                }
-
-                switch (userCommand)
-                {
-                    case "/start":
-                        userName = Start();
-                        break;
-                    case "/help":
-                        Help(userName);
-                        break;
-                    case "/info":
-                        Info(userName);
-                        break;
-                    case "/exit":
-                        return;
-                    default:
-                        Console.WriteLine("The command is anavailable." +
-                            Environment.NewLine +
-                            " ");
-                        break;
-                }
+                Echo(userName, userCommand);
+                continue;
             }
-        }
 
-        static void Greeting(string userName)
-        {
-            string botGreeting = "Welcome to the bot's interactive menu";
-
-            string botCommandsList = "Use the following commands to communicate with the bot:" +
-                Environment.NewLine +
-                "/start" +
-                Environment.NewLine +
-                "/help" +
-                Environment.NewLine +
-                "/info" +
-                Environment.NewLine +
-                "/exit";
-
-            if (string.IsNullOrEmpty(userName))
+            switch (userCommand)
             {
-                Console.WriteLine($"{botGreeting}." +
-                    Environment.NewLine +
-                    $"{botCommandsList}");
-            }
-            else
-            {
-                Console.WriteLine($"{botGreeting}, {userName}." +
-                    Environment.NewLine +
-                    $"{botCommandsList}");
-            }
-        }
-
-        static string Start()
-        {
-            Console.WriteLine("Input your name, please.");
-
-            string name = Console.ReadLine();
-
-            if (string.IsNullOrEmpty(name))
-            {
-                Console.WriteLine("Name can't be null." +
-                    Environment.NewLine +
-                            " ");
-            }
-            else
-            {
-                Console.WriteLine($"Hello, {name}. The command /echo is available for you now." +
+                case "/start":
+                    userName = Start();
+                    break;
+                case "/help":
+                    Help(userName);
+                    break;
+                case "/info":
+                    Info(userName);
+                    break;
+                case "/exit":
+                    return;
+                default:
+                    Console.WriteLine("The command is anavailable." +
                         Environment.NewLine +
                         " ");
-            }
-
-            return name;
-        }
-
-        static void Help(string userName)
-        {
-            string helpTextHeading = "Here you can find more detailed information about available commands";
-
-            string helpText = "- /start: Restarts the bot and initializes your session." +
-                Environment.NewLine +
-                "- /info: Displays an information about the program's version and it's creation date." +
-                Environment.NewLine +
-                "- /help: Provides information on how to use the bot." +
-                Environment.NewLine +
-                "- /echo: Echoes back the text you enter after this command. Note: The command is only available after you have used the command /start and provided your name." +
-                Environment.NewLine +
-                "- /exit: Exits the program." +
-                Environment.NewLine +
-                " ";
-
-            if (string.IsNullOrEmpty(userName))
-            {
-                Console.WriteLine($"{helpTextHeading}:" +
-                    Environment.NewLine +
-                    $"{helpText}" +
-                    Environment.NewLine +
-                    " ");
-            }
-            else
-            {
-                Console.WriteLine($"{helpTextHeading}, {userName}:" +
-                    Environment.NewLine +
-                    $"{helpText}" +
-                    Environment.NewLine +
-                    " ");
+                    break;
             }
         }
+    }
 
-        static void Info(string userName)
+    static void Greeting(string userName)
+    {
+        string botGreeting = "Welcome to the bot's interactive menu";
+
+        string botCommandsList = "Use the following commands to communicate with the bot:" +
+            Environment.NewLine +
+            "/start" +
+            Environment.NewLine +
+            "/help" +
+            Environment.NewLine +
+            "/info" +
+            Environment.NewLine +
+            "/exit";
+
+        if (string.IsNullOrEmpty(userName))
         {
-            string infoText = $"Version: 1.0.0" +
+            Console.WriteLine($"{botGreeting}." +
                 Environment.NewLine +
-                "Creation Date: 17/11/2024";
+                $"{botCommandsList}");
+        }
+        else
+        {
+            Console.WriteLine($"{botGreeting}, {userName}." +
+                Environment.NewLine +
+                $"{botCommandsList}");
+        }
+    }
 
-            if (string.IsNullOrEmpty(userName))
-            {
-                Console.WriteLine($"{infoText}" +
+    static string Start()
+    {
+        Console.WriteLine("Input your name, please.");
+
+        string name = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(name))
+        {
+            Console.WriteLine("Name can't be null." +
+                Environment.NewLine +
+                        " ");
+        }
+        else
+        {
+            Console.WriteLine($"Hello, {name}. The command /echo is available for you now." +
                     Environment.NewLine +
                     " ");
-            }
-            else
-            {
-                Console.WriteLine($"{userName}," +
-                    Environment.NewLine +
-                    $"{infoText}" +
-                    Environment.NewLine +
-                    " ");
-            }
         }
 
-        static void Echo(string userName, string userText)
-        {
-            if (string.IsNullOrEmpty(userName))
-            {
-                Console.WriteLine("Sorry, the command is not available now. Use the command /start instead." +
-                    Environment.NewLine +
-                    " ");
-            }
-            else
-            {
-                string echoText = userText.Substring(5);
+        return name;
+    }
 
-                if (echoText.Length > 0)
+    static void Help(string userName)
+    {
+        string helpTextHeading = "Here you can find more detailed information about available commands";
+
+        string helpText = "- /start: Restarts the bot and initializes your session." +
+            Environment.NewLine +
+            "- /info: Displays an information about the program's version and it's creation date." +
+            Environment.NewLine +
+            "- /help: Provides information on how to use the bot." +
+            Environment.NewLine +
+            "- /echo: Echoes back the text you enter after this command. Note: The command is only available after you have used the command /start and provided your name." +
+            Environment.NewLine +
+            "- /exit: Exits the program." +
+            Environment.NewLine +
+            " ";
+
+        if (string.IsNullOrEmpty(userName))
+        {
+            Console.WriteLine($"{helpTextHeading}:" +
+                Environment.NewLine +
+                $"{helpText}" +
+                Environment.NewLine +
+                " ");
+        }
+        else
+        {
+            Console.WriteLine($"{helpTextHeading}, {userName}:" +
+                Environment.NewLine +
+                $"{helpText}" +
+                Environment.NewLine +
+                " ");
+        }
+    }
+
+    static void Info(string userName)
+    {
+        string infoText = $"Version: 1.0.0" +
+            Environment.NewLine +
+            "Creation Date: 17/11/2024";
+
+        if (string.IsNullOrEmpty(userName))
+        {
+            Console.WriteLine($"{infoText}" +
+                Environment.NewLine +
+                " ");
+        }
+        else
+        {
+            Console.WriteLine($"{userName}," +
+                Environment.NewLine +
+                $"{infoText}" +
+                Environment.NewLine +
+                " ");
+        }
+    }
+
+    static void Echo(string userName, string userText)
+    {
+        if (string.IsNullOrEmpty(userName))
+        {
+            Console.WriteLine("Sorry, the command is not available now. Use the command /start instead." +
+                Environment.NewLine +
+                " ");
+        }
+        else
+        {
+            string echoText = userText.Substring(5);
+
+            if (echoText.Length > 0)
+            {
+                if (echoText[0] == ' ')
                 {
-                    if (echoText[0] == ' ')
+                    if (string.IsNullOrEmpty(echoText.Substring(1)))
                     {
-                        if (string.IsNullOrEmpty(echoText.Substring(1)))
-                        {
-                            Console.WriteLine("The text after /echo can't be empty." +
-                                Environment.NewLine +
-                                " ");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"{userName}, your text is {echoText.Trim()}." +
-                                Environment.NewLine +
-                                " ");
-                        }
+                        Console.WriteLine("The text after /echo can't be empty." +
+                            Environment.NewLine +
+                            " ");
                     }
                     else
                     {
-                        Console.WriteLine("The command is unavalaible." +
+                        Console.WriteLine($"{userName}, your text is {echoText.Trim()}." +
                             Environment.NewLine +
                             " ");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Make sure you entered some text after the command. For example: /echo <your text is here>." +
+                    Console.WriteLine("The command is unavalaible." +
                         Environment.NewLine +
                         " ");
                 }
+            }
+            else
+            {
+                Console.WriteLine("Make sure you entered some text after the command. For example: /echo <your text is here>." +
+                    Environment.NewLine +
+                    " ");
             }
         }
     }
