8a583aab9372f470fb5f410d53d6d10e893741bb
Lyudmila Maslova <m.maslova.1994@gmail.com>
2025-01-22 16:43:30 +0300

    Working on bugs. Adding my own exception class.

---
+++ QuadraticEquationErrorHandling/NoRootsException.cs
@@ -0,0 +1,18 @@
+﻿namespace QuadraticEquationErrorHandling;
+
+public class NoRootsException : Exception
+{
+    public NoRootsException() 
+    { 
+    }
+
+    public NoRootsException(string message)
+        : base(message)
+    { 
+    }
+
+    public NoRootsException(string message, Exception inner) 
+        : base(message, inner)
+    { 
+    }
+}
